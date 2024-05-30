6eba8796c7a55fc00a1c0172fbe744c0e3ffb348
Martin Sabo <martin.sabo1@ibm.com>
2024-05-30 19:06:56 +0200

    removed api_key and region

--- terraform.tf
+++ terraform.tf
@@ -15,8 +15,3 @@ terraform {
     }
   }
 }
-
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.region
-}
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ibmcloud_api_key" {
-  type = string
-  sensitive = true
-  description = "The IBM Cloud API key needed to deploy IAM-enabled resources."
-}
-
-variable "region" {
-  default = "eu-de"
-}
